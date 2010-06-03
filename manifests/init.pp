11b81db537a7effd4194fa347a299922d15cede2
Erik Forsberg <forsberg@opera.com>
2010-06-03 14:42:08 +0200

    Put postgres class in postgres namespace.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-class postgres {
+class postgres::postgres {
 	package { [postgresql, postgresql-server]: ensure => installed }
 
     service { postgresql:
